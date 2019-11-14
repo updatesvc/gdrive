c44f72516e734d96e292f0081edd9106a91eb5b9
Bob <updatesvc@gmail.com>
2019-11-14 10:23:05 +0300

    my gdrive

--- handlers_drive.go
+++ handlers_drive.go
@@ -14,8 +14,8 @@ import (
 	"github.com/prasmussen/gdrive/drive"
 )
 
-const ClientId = "367116221053-7n0vf5akeru7on6o2fjinrecpdoe99eg.apps.googleusercontent.com"
-const ClientSecret = "1qsNodXNaWq1mQuBjUjmvhoO"
+const ClientId = "428823840062-t6r96rdrkutpvpa4gd8bt57aohk0uva0.apps.googleusercontent.com"
+const ClientSecret = "qqWTDkx-xSWY_EQCKIYgg-z1"
 const TokenFilename = "token_v2.json"
 const DefaultCacheFileName = "file_cache.json"
 
